6933b67aee07b149723688a7f332ff2265948d3c
Milan Santosi <milan.santosi@gmail.com>
2025-01-15 03:20:53 +0100

    bugfix

--- static/js/leistungen.js
+++ static/js/leistungen.js
@@ -1,20 +1,8 @@
 $(document).ready(function() {
-    // Close on outside clicks
-    $(document).on('click', function(e) {
-        if (!$(e.target).closest('.dropdown').length) {
-            $('.dropdown').removeClass('open');
+    $('.nav a').on('click', function() {
+        if ($('.navbar-collapse').hasClass('in')) {
             $('.navbar-collapse').collapse('hide');
         }
-    });
-
-    // Close on any nav item click
-    $('.navbar-nav a').on('click', function() {
-        $('.navbar-collapse').collapse('hide');
-    });
-
-    // Handle burger toggle
-    $('.js-fh5co-nav-toggle').on('click', function() {
-        $('.navbar-collapse').toggleClass('in');
     });
 
     function scrollAndOpen(sectionId, event) {
