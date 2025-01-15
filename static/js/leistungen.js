8fa1025655e9223ea6326c3a1cb0db04cde64434
Milan Santosi <milan.santosi@gmail.com>
2025-01-15 03:11:09 +0100

    also fix burger menu

--- static/js/leistungen.js
+++ static/js/leistungen.js
@@ -1,26 +1,28 @@
 $(document).ready(function() {
-    // Close dropdown when clicking outside
+    // Handle both desktop and mobile dropdowns
     $(document).on('click', function(e) {
         if (!$(e.target).closest('.dropdown').length) {
             $('.dropdown').removeClass('open');
+            $('.navbar-collapse').collapse('hide');
         }
+    });
+
+    // Toggle mobile menu on burger click
+    $('.js-fh5co-nav-toggle').on('click', function() {
+        $('.navbar-collapse').toggleClass('in');
     });
 
     function scrollAndOpen(sectionId, event) {
         event.preventDefault();
 
-        // Close Bootstrap dropdown
+        // Close all dropdowns and mobile menu
         $('.dropdown').removeClass('open');
-
-        // Close mobile menu
         $('.navbar-collapse').collapse('hide');
 
-        // Scroll and toggle section
         const section = document.querySelector('.leistungen-section');
         section.scrollIntoView({ behavior: 'smooth' });
         setTimeout(() => toggleSection(sectionId), 500);
     }
 
-    // Make scrollAndOpen available globally
     window.scrollAndOpen = scrollAndOpen;
-});
+})
