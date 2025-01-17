6831307536c3a3ffa8caa1e65ff713f8fe32faec
Milan Santosi <milan.santosi@gmail.com>
2025-01-17 19:27:32 +0100

    video-init js

---
+++ static/js/video-init.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // Initialize popup video
+    $('.popup-vimeo').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false
+    });
+});
