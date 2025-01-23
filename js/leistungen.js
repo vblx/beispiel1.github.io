a70e6db6e9d8f533b763f854126420317a6ac122
M1lan <M1lan@users.noreply.github.com>
2025-01-23 15:46:10 +0000

    deploy: 49e0c625a7763efe56038bc30421722befd3815b

--- js/leistungen.js
+++ js/leistungen.js
@@ -1,51 +1,19 @@
-
 $(document).ready(function() {
+    // Close mobile navbar on link click
     $('.nav a').on('click', function() {
         if ($('.navbar-collapse').hasClass('in')) {
             $('.navbar-collapse').collapse('hide');
         }
     });
 
+    // Handle scroll and section opening
     function scrollAndOpen(sectionId, event) {
         event.preventDefault();
-        $('.dropdown').removeClass('open');
-        $('.navbar-collapse').collapse('hide');
-
+        // Let Bootstrap handle the dropdown closing
         const section = document.querySelector('.leistungen-section');
         section.scrollIntoView({ behavior: 'smooth' });
         setTimeout(() => toggleSection(sectionId), 500);
     }
 
-    $('.dropdown-toggle').on('click', function() {
-        const arrow = this.querySelector('.icon-arrow-down');
-        if (arrow) {
-            arrow.classList.toggle('rotated');
-        }
-    });
-
     window.scrollAndOpen = scrollAndOpen;
 });
-
-
-
-
-
-// $(document).ready(function() {
-//     // Close mobile navbar on link click
-//     $('.nav a').on('click', function() {
-//         if ($('.navbar-collapse').hasClass('in')) {
-//             $('.navbar-collapse').collapse('hide');
-//         }
-//     });
-
-//     // Handle scroll and section opening
-//     function scrollAndOpen(sectionId, event) {
-//         event.preventDefault();
-//         // Let Bootstrap handle the dropdown closing
-//         const section = document.querySelector('.leistungen-section');
-//         section.scrollIntoView({ behavior: 'smooth' });
-//         setTimeout(() => toggleSection(sectionId), 500);
-//     }
-
-//     window.scrollAndOpen = scrollAndOpen;
-// });
