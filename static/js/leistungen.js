74d4d3e5d2fd24e4d3bbc1676f14ca432fd9046d
Milan Santosi <milan.santosi@gmail.com>
2025-01-24 08:37:54 +0100

    update2

--- static/js/leistungen.js
+++ static/js/leistungen.js
@@ -1,19 +1,178 @@
-$(document).ready(function() {
-    // Close mobile navbar on link click
-    $('.nav a').on('click', function() {
-        if ($('.navbar-collapse').hasClass('in')) {
-            $('.navbar-collapse').collapse('hide');
-        }
-    });
+// Replace entire content of leistungen.js with:
+$(document).ready(function(){
+  const $dropdown = $('.dropdown');
+  const $dropdownToggle = $('.dropdown-toggle');
 
-    // Handle scroll and section opening
-    function scrollAndOpen(sectionId, event) {
-        event.preventDefault();
-        // Let Bootstrap handle the dropdown closing
-        const section = document.querySelector('.leistungen-section');
-        section.scrollIntoView({ behavior: 'smooth' });
-        setTimeout(() => toggleSection(sectionId), 500);
+  // Close on page click
+  $(document).on('click', function(e) {
+    if (!$(e.target).closest('.dropdown').length) {
+      $dropdown.removeClass('open');
     }
+  });
 
-    window.scrollAndOpen = scrollAndOpen;
+  // Toggle on dropdown click
+  $dropdownToggle.on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $dropdown.toggleClass('open');
+  });
+
+  // Close on navbar items
+  $('.nav-link').not($dropdownToggle).on('click', function() {
+    $dropdown.removeClass('open');
+  });
+
+  // Update scrollAndOpen to close dropdown
+  window.scrollAndOpen = function(sectionId, event) {
+    event.preventDefault();
+    const section = document.querySelector('.leistungen-section');
+    section.scrollIntoView({ behavior: 'smooth' });
+    setTimeout(() => toggleSection(sectionId), 500);
+    $dropdown.removeClass('open');
+  }
 });
+
+
+
+//// graveyard below
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+// 	// Handle scroll and section opening
+//     function scrollAndOpen(sectionId, event) {
+//         event.preventDefault();
+//         const section = document.querySelector('.leistungen-section');
+//         section.scrollIntoView({ behavior: 'smooth' });
+//         setTimeout(() => toggleSection(sectionId), 500);
+//     }
+
+//     window.scrollAndOpen = scrollAndOpen;
+// });
+
+// $(document).ready(function(){
+// 	// Initialize dropdown
+// 	$('.dropdown-toggle').dropdown();
+
+// 	window.scrollAndOpen = function(sectionId, event) {
+// 		event.preventDefault();
+// 		const section = document.querySelector('.leistungen-section');
+// 		section.scrollIntoView({ behavior: 'smooth' });
+// 		setTimeout(() => toggleSection(sectionId), 500);
+
+// 		// Close dropdown after clicking menu item
+// 		$('.dropdown-toggle').dropdown('toggle');
+// 	}
+// });
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//  const $dropdown = $('.dropdown-toggle');
+
+//  // Close on page load
+//  $('.dropdown-menu').hide();
+
+//  // Toggle on "Leistungen" click
+//  $dropdown.click(function(e){
+//    e.preventDefault();
+//    $(this).parent().toggleClass('open');
+//  });
+
+//  // Close on navbar items click
+//  $('.nav-link').not($dropdown).click(function(){
+//    $('.dropdown').removeClass('open');
+//  });
+
+//  // Keep original scrollAndOpen
+//  window.scrollAndOpen = function(sectionId, event) {
+//    event.preventDefault();
+//    const section = document.querySelector('.leistungen-section');
+//    section.scrollIntoView({ behavior: 'smooth' });
+//    setTimeout(() => toggleSection(sectionId), 500);
+//  }
+// });
+
+
+
+// $(document).ready(function(){
+//   const $dropdown = $('.dropdown');
+//   const $dropdownToggle = $('.dropdown-toggle');
+
+//   $dropdownToggle.click(function(e){
+//     e.preventDefault();
+//     e.stopPropagation();
+//     $dropdown.toggleClass('open');
+//   });
+
+//   $('.nav-link').not($dropdownToggle).click(function(){
+//     $dropdown.removeClass('open');
+//   });
+
+//   window.scrollAndOpen = function(sectionId, event) {
+//     event.preventDefault();
+//     const section = document.querySelector('.leistungen-section');
+//     section.scrollIntoView({ behavior: 'smooth' });
+//     setTimeout(() => toggleSection(sectionId), 500);
+//     $dropdown.removeClass('open');
+//   }
+// });
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//  const $dropdown = $('.dropdown');
+//  const $dropdownToggle = $('.dropdown-toggle');
+
+//  $dropdownToggle.click(function(e){
+//    e.preventDefault();
+//    e.stopPropagation();
+//    $dropdown.toggleClass('open');
+//  });
+
+//  $('.nav-link').not($dropdownToggle).click(function(){
+//    $dropdown.removeClass('open');
+//  });
+
+//  window.scrollAndOpen = function(sectionId, event) {
+//    event.preventDefault();
+//    const section = document.querySelector('.leistungen-section');
+//    section.scrollIntoView({ behavior: 'smooth' });
+//    setTimeout(() => toggleSection(sectionId), 500);
+//    $dropdown.removeClass('open');
+//  }
+// });
+
+
+
+
+// $(document).ready(function(){
+//  const $dropdown = $('.dropdown');
+//  const $dropdownToggle = $('.dropdown-toggle');
+
+//  $dropdownToggle.click(function(e){
+//    e.preventDefault();
+//    e.stopPropagation();
+//    $dropdown.toggleClass('open');
+//  });
+
+//  $('.nav-link').not($dropdownToggle).click(function(){
+//    $dropdown.removeClass('open');
+//  });
+
+//  window.scrollAndOpen = function(sectionId, event) {
+//    event.preventDefault();
+//    const section = document.querySelector('.leistungen-section');
+//    section.scrollIntoView({ behavior: 'smooth' });
+//    setTimeout(() => toggleSection(sectionId), 500);
+//    $dropdown.removeClass('open');
+//  }
+// });
